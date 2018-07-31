cf27cb79c714b2f9b0bf64b23573988fb052e060
sandi87 <sndrstank@gmail.com>
2018-07-31 15:36:10 +0200

    this add test checking if name is defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,8 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Wr
-        ite a test that loops through each feed
+        /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -45,6 +44,12 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name defined', function() {
+           allFeeds.forEach(function(feed) {
+             expect(feed.name).toBeDefined();
+             expect(feed.name.length).not.toBe(0);
+           });
+         });
     });
 
 
