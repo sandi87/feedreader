b581a8137642eb63af77949581cb79abff23c54e
sandi87 <sndrstank@gmail.com>
2018-08-01 10:46:46 +0200

    this test checks if there is at least one singe element in .feed container

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -82,8 +82,6 @@ $(function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
       describe('Initial Enteries', function() {
-        
-      })
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -91,11 +89,32 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+           loadFeed(0, function() {
+             done();
+           });
+         });
+         it('is at least a single .entry element within .feed container', function(done) {
+           loadFeed(1, function() {
+             expect($('.feed .entry').length).not.toBe(0);
+             done();
+           });
+         });
+
+      });
+
+
   });
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+      
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+
+
+  });
 }());
